fe57cdeb9ccea4f564778ed5d06b802b7a6c3c6e
Moe-Karim <mohamadkarimfahs@gmail.com>
2024-10-28 00:07:12 +0200

    Created: js files, added variables used to the tool.js

---
+++ scripts/myTools.js
@@ -0,0 +1,6 @@
+var status = document.getElementById('status');
+var game = document.getElementById('game')
+var start = document.getElementById('start');
+var boundary = document.querySelectorAll('.boundary');
+var end = document.getElementById('end');
+var win = true;
