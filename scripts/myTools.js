d4e9275c68a1b2a564e623954fa7837f9724c1cf
Moe-Karim <mohamadkarimfahs@gmail.com>
2024-10-28 00:08:50 +0200

    Added: Game start,end,win and loose functions

--- scripts/myTools.js
+++ scripts/myTools.js
@@ -4,3 +4,34 @@ var start = document.getElementById('start');
 var boundary = document.querySelectorAll('.boundary');
 var end = document.getElementById('end');
 var win = true;
+
+function lost(){
+    document.getElementById("status").innerHTML="You Lost! :(";
+    document.getElementById("status").style.color='red';
+    document.getElementById('game').style.backgroundColor='red';
+    win = false;
+    GameEnd();
+}
+function won(){
+    document.getElementById("status").innerHTML="You Win! :)";
+    document.getElementById("status").style.color='green';
+    document.getElementById('game').style.backgroundColor='green';
+    win = true;
+    GameEnd();
+}
+function GameEnd(){
+    start.removeEventListener('mouseover',GameStart);
+    game.removeEventListener('mouseleave',Cheating);
+    boundary.forEach((element)=>{
+        element.removeEventListener('mouseover',boundaries);
+    });
+    end.removeEventListener('mouseover',YouWin);
+    setTimeout(()=>{location.reload();},1000);
+
+}
+function GameStart() {
+    document.getElementById('status').innerHTML = 'Try not to hit the borders!';
+    game.addEventListener('mouseleave', Cheating);
+    boundaries();
+    YouWin();
+}
